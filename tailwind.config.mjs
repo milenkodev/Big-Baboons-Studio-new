7da152be1f0f5e7a3e3528c7c3558add7879e630
milodev <milenko.ilic18@gmail.com>
2024-05-21 12:45:45 +0200

    add animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,20 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 2s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform:
+              "translateX(calc(-100% - clamp(0.75rem, 0.2237rem + 2.6316vw, 2rem)))",
+          },
+        },
+      },
       colors: {
         white: "#F3F3F3",
         black: "#1C1C1C",
