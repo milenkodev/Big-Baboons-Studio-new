5b3cd4a39f37a57e62f9cf75fea4d60f54c4f88c
milodev <milenko.ilic18@gmail.com>
2024-06-03 08:47:16 +0200

    add routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
- i18n: {
-   defaultLocale: 'nl',
-   locales: ['en', 'nl'],
+  i18n: {
+    defaultLocale: "nl",
+    locales: ["en", "nl"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
   },
-  integrations: [tailwind(), alpinejs(), ],
+  integrations: [tailwind(), alpinejs()],
   image: {
-    domains: ['res.cloudinary.com']
-  }
+    domains: ["res.cloudinary.com"],
+  },
 });
